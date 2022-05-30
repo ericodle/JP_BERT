82c4461e49798503d5dcf8b42214ead6f8be0977
EricOdle <72859331+ericodle@users.noreply.github.com>
2022-05-30 16:50:22 +0800

    Update generate_ppx.py

--- generate_ppx.py
+++ generate_ppx.py
@@ -22,27 +22,38 @@ Here we define the masking algorithm that calculates perplexity.
 """
 
     for i, word in enumerate(tokenize_input):
+        
         # The input [MASK] is recognized by BERT as a masking token.
         tokenize_input[i] = '[MASK]'
         mask_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
         output = model(mask_input)
         prediction_scores = output[0]
+        
+        # See our published paper for more details on why we use Softmax.
         softmax = nn.Softmax(dim=0)
         ps = softmax(prediction_scores[0, i]).log()
         word_loss = ps[tensor_input[0, i]]
         sentence_loss += word_loss.item()
         tokenize_input[i] = word
+        
     # Perplexity (ppl) is defined as the exponential of the negative ratio of sentence loss to sentence length.
     ppl = np.exp(-sentence_loss/sen_len)
     return ppl
 
-sentences_file = pd.read_csv ('./N3_adverbs.csv')
-sentences = pd.DataFrame(sentences_file, columns=['text', 'perplexity'])
+test_text = pd.read_csv ('./N3_adverbs.csv')
+sentences = pd.DataFrame(test_text, columns=['text', 'perplexity'])
 
 output=[]
 for i in range(0, len(sentences.text)):
     result=run_bert(sentences.text[i])
     output.append(result)
-
+    
 print(output)
+
+"""
+We simply printed the resulting values in the terminal emulator and copy-pasted it to a CSV for later alignment with the source text.
+This was slow but manageable, as each blank space in sentences.text produced an outlier for alignment reference.
+Feel free to revise the code so the ppx values are written directly into the pandas dataframe "perplexity" column.
+"""
+
 
